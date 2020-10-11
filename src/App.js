c271724cc16c114a9bf2cfc4164c75a88f7de535
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-11 22:52:21 +0100

    display hook result

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import { Container } from 'react-bootstrap';
 
 function App() {
   const { jobs, loading, error } = useFetchJobs();
-  return <Container></Container>;
+  return (
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error try reloading...</h1>}
+      {jobs && jobs.length}
+    </Container>
+  );
 }
 
 export default App;
