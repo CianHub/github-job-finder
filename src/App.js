4257c390da4cb2ec1f0f2f2327d8f7dada4f446c
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-11 22:51:08 +0100

    Add React bootstrap contaner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 import useFetchJobs from './hooks/useFetchJobs';
+import { Container } from 'react-bootstrap';
 
 function App() {
   const { jobs, loading, error } = useFetchJobs();
-  return <div className="App"></div>;
+  return <Container></Container>;
 }
 
 export default App;
