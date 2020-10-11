3319c49d3f6af2d275f729fa8ad2fc6ecf10110d
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-11 22:47:56 +0100

    create custom useFetchJobs hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
+import useFetchJobs from './hooks/useFetchJobs';
 
 function App() {
+  const jobs = useFetchJobs();
   return <div className="App"></div>;
 }
 
